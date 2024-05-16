624be4774fd2790971c4362a20dff4088d2428e7
Oguzhan Yıldız <oguzhan.yildiz@32bit.com.tr>
2024-05-16 21:42:38 +0300

    deleted project for security

--- src/main/java/com/sau/dims/repository/AdviserStudyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sau.dims.repository;
-
-import com.sau.dims.model.AdviserStudy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdviserStudyRepository extends JpaRepository<AdviserStudy,Integer> {
-}
