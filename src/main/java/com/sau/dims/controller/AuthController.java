24689335de291a1702f6721adbce9484973bc894
Yunus Emre <yunustc5461@gmail.com>
2024-05-19 14:59:57 +0300

    adviser register validation

--- src/main/java/com/sau/dims/controller/AuthController.java
+++ src/main/java/com/sau/dims/controller/AuthController.java
@@ -6,15 +6,17 @@ import com.sau.dims.dto.UserDTO;
 import com.sau.dims.security.AuthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@RestController
+@Controller
 @RequestMapping("/api/auth")
 public class AuthController {
 
@@ -22,13 +24,31 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/register")
-    public void  register(@ModelAttribute UserDTO user,HttpServletResponse response) throws IOException {
-        authService.register(user);
-        response.sendRedirect("/login");
+    public String  register(@Valid @ModelAttribute("user") UserDTO user, BindingResult result, Model model) throws IOException {
+        System.out.println("func");
+        if(result.hasErrors()){
+            model.addAttribute("user",user);
+            model.addAttribute("error",result.getAllErrors());
+            return "/auth/signupPage";
+        }
+
+        boolean isRegist = authService.register(user);
+        System.out.println(isRegist);
+        if(!isRegist){
+            System.out.println("hataaaa");
+            ObjectError error = new ObjectError("globalError", "Username already exist!");
+//            model.addAttribute("error",error);
+            model.addAttribute("globalError","Username already exist!");
+            result.addError(error);
+            return "/auth/signupPage";
+        }
+
+        return "redirect:/login";
     }
 
 
     @PostMapping("/login")
+    @ResponseBody
     public void login(@ModelAttribute LoginDTO loginDTO, HttpServletResponse response) throws IOException {
         AuthResponseDTO authResponseDTO = authService.login(loginDTO);
         Cookie authCookie = new Cookie("Authorization", authResponseDTO.getAccessToken());
--- src/main/java/com/sau/dims/dto/UserDTO.java
+++ src/main/java/com/sau/dims/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package com.sau.dims.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +11,21 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
-
+    @NotBlank(message = "Username can not be null!")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Username can not contain any non-letter!")
     private String username;
+    @NotBlank(message = "Password name can not be null!")
+    @Pattern(regexp = "^[a-zA-Z1-9]*$", message = "Password only can be among of a-zA-Z1-9")
     private String password;
+    @NotBlank(message = "Adviser's name can not be null!")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Name can not contain any non-letter!")
     private String name;
+    @NotBlank(message = "Adviser's surname can not be null!")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Surname can not contain any non-letter!")
     private String surname;
     private String role;
     private MultipartFile picture;
+    @NotBlank(message = "Department can not be null!")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Department can not contain any non-letter!")
     private String department;
 }
